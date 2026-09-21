4a25ead575f3f4e749173d3130092cf0491cdcae
karthikeyannkl <78947335+karthikeyannkl@users.noreply.github.com>
2026-09-21 12:09:43 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ const DEFAULT_STATE = {
   magizhB5: {},
   magizhCoinWallet: "0",
   magizhAdminPassword: null,
-  magizhLevelTrackingV1: {},
   magizhCurrentUser: null,
   magizhCurrentUserId: "GUEST"
 };
